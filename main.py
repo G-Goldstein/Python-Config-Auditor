488a57e11f5c6612c11a13bce20d9b2f4ec62c51
Graham Goldstein <g.goldstein@live.com>
2015-11-20 11:28:04 +0000

    Parameterised the request for collection and target data library

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ import output
 import i_tables
 import timestamp
 import run_selection
+import sys
 
-run_time = timestamp.time_string()
-configured_environments = run_selection.configured_environments()
+def collect_audit_for_environment(environment, source_path, save_directory):
+	print('{!s} at {!s}'.format(environment, source_path))
+	run_time = timestamp.time_string()
 
-for environment in configured_environments:
-
-	print('{!s} at {!s}'.format(environment, configured_environments[environment]))
-
-	source_path = configured_environments[environment]
-	save_file = '../data/{!s}_{!s}.json'.format(run_time, environment)
+	save_file = '{!s}/{!s}_{!s}.json'.format(save_directory, run_time, environment)
 	overview = {'Environment':environment, 'Date':timestamp.current_date(), 'Time':timestamp.current_time()}
 	comparison_object = {'overview':overview, 'config_files':[], 'database_tables':{}}
 
@@ -28,4 +25,20 @@ for environment in configured_environments:
 
 	i_tables.get_i_table_data(comparison_object)
 
-	output.write_out_json(comparison_object, save_file)
+	output.write_out_json(comparison_object, save_file, save_directory)
+
+def collect_audit_for_all_environments(save_directory):
+	configured_environments = run_selection.configured_environments()
+	for environment in configured_environments:
+		collect_audit_for_environment(environment, configured_environments[environment], save_directory)
+		
+
+if __name__ == "__main__":
+	try:
+		environment = sys.argv[1]
+		source_path = sys.argv[2]
+		save_directory = sys.argv[3]
+	except:
+		print('Bad parameters passed')
+	else:
+		collect_audit_for_environment(environment, source_path, save_directory)
--- output.py
+++ output.py
@@ -1,6 +1,6 @@
 import json
 
-def write_out_json(comparison_object, save_path):
+def write_out_json(comparison_object, save_path, save_directory):
 	with open(save_path, 'w') as file:
 		file.write(json.dumps(comparison_object, sort_keys=True, indent=4, separators=(',', ': ')))
 	print('Done! Audit output to ' + save_path)
