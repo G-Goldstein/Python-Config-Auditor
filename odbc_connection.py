27b23b88145c44b07adab3ceac949a064235a2b8
Graham Goldstein <g.goldstein@live.com>
2015-12-18 11:40:37 +0000

    Moved the connection DSN into an environment variable

--- odbc_connection.py
+++ odbc_connection.py
@@ -1,4 +1,5 @@
 import pyodbc
+import os
 
 i_connection_properties_required = ['iseries.username', 'iseries.password', 'iseries.connection', 'iseries.librarylist']
 
@@ -16,7 +17,7 @@ class odbc_connect:
 		self.user = args['iseries.username']
 		self.libl = args['iseries.librarylist']
 		self.libarray = self.libl.split(',')
-		self.connection_string="DSN=DB2DSN;ssl=1;System={!s};Uid={!s};Pwd={!s};DefaultLibraries={!s};ConnectionType=2".format(self.ip, self.user, args['iseries.password'], self.libl)
+		self.connection_string="DSN={!s};ssl=1;System={!s};Uid={!s};Pwd={!s};DefaultLibraries={!s};ConnectionType=2".format(os.environ['dsn'], self.ip, self.user, args['iseries.password'], self.libl)
 
 	def __enter__(self):
 		print(self.connection_string)
